40169eaaf152521125da2744ed830d04eab478f1
rfinn <rfinn>
2023-07-11 13:23:58 -0400

    updates

--- halphamain.py
+++ halphamain.py
@@ -1691,13 +1691,13 @@ class hafunctions(Ui_MainWindow, create_output_table, uco_table):
         ##
         # SKIPPING MIN/MAX RESET - would rather correct values in source catalogs
         ##
-        #for i,s in enumerate(self.cutout_sizes_arcsec):
-        #    if s > self.global_max_cutout_size:
-        #        self.cutout_sizes_arcsec[i] = self.global_max_cutout_size
-        #        self.cutout_sizes[i] = self.global_max_cutout_size.value/self.pixelscale
-        #    elif s < self.global_min_cutout_size:
-        #        self.cutout_sizes_arcsec[i] = self.global_min_cutout_size
-        #        self.cutout_sizes[i] = self.global_min_cutout_size.value/self.pixelscale                
+        for i,s in enumerate(self.cutout_sizes_arcsec):
+            if s > self.global_max_cutout_size:
+                self.cutout_sizes_arcsec[i] = self.global_max_cutout_size
+                self.cutout_sizes[i] = self.global_max_cutout_size.value/self.pixelscale
+            elif s < self.global_min_cutout_size:
+                self.cutout_sizes_arcsec[i] = self.global_min_cutout_size
+                self.cutout_sizes[i] = self.global_min_cutout_size.value/self.pixelscale                
         #print('after comparing with max/min size limits:')
         #print('\t cutout sizes arcsec = ',self.cutout_sizes_arcsec)        
         # set up the output table that will store results from various fits
