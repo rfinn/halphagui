9653287569ebce03de07e61eea176a11b9c6ae8a
rfinn <rfinn>
2023-08-24 17:08:33 -0400

    fixing crap

--- photwrapper.py
+++ photwrapper.py
@@ -624,7 +624,11 @@ class ellipse():
         self.im1_skynoise = sky_noise_erg
         # get sky noise for image 2
         if self.image2 is not None:
-
+            sky_noise_erg2 = self.sky2_noise*self.uconversion2/self.pixel_scale**2
+            self.header2.set('PHOT_SKY','{:.2f}'.format(self.sky2),'sky in ADU')
+            self.header2.set('SKYNOISE','{:.2f}'.format(self.sky2_noise),'sky noise in ADU')        
+            self.header2.set('SKYERR','{:.2e}'.format(sky_noise_erg2),'sky noise in erg/s/cm^2/arcsec^2')
+            fits.writeto(self.image2_name,self.image2,header=self.header2,overwrite=True
             try:
                 sky_noise_erg2 = self.sky2_noise*self.uconversion2/self.pixel_scale**2
                 self.header2.set('PHOT_SKY','{:.2f}'.format(self.sky2),'sky in ADU')
