3afbcf5d0976755216bf2c4a41c727c5e85f8146
rfinn <rfinn@siena.edu>
2026-03-08 13:48:25 -0400

    - removing star rejection restriction

--- buildpsf.py
+++ buildpsf.py
@@ -72,7 +72,7 @@ class psf_parent_image():
             self.sepath=os.getenv('HOME')+'/github/halphagui/astromatic/'
         else:
             self.sepath = sepath
-        # number of stars to use to determine the psf
+        # number of stars touse to determine the psf
         self.nstars = nstars
         # default pixelscale is set for HDI camera
 
@@ -223,7 +223,7 @@ class psf_parent_image():
             # some pixels have values == 0 but they don't appear to be bad?  or are they masked?
             # increasing cut from 1 to 10 to compensate for the case where maybe 1-2 pixels are bad
             # still loosing a lot of stars so bumping up cut to 100
-            if len(np.where(s.data == 0)[0]) > 100:
+            if len(np.where(s.data == 0)[0]) > 1000:
                 keepflag[i] = False
 
         print(f"number of stars to keep after ==0 cut = {np.sum(keepflag)}/{len(keepflag)}")
