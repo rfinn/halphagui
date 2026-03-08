1c2900305c80dd1bd287e2e5f3e5af946cd3f844
rfinn <rfinn@siena.edu>
2026-03-08 18:39:36 -0400

    - removing star rejection restriction

--- filterratio.py
+++ filterratio.py
@@ -72,8 +72,9 @@ def run_sextractor(image1,image2, default_se_dir = 'halphagui/astromatic'):
     secatalog2 = f"{catdir}/{froot2}.cat"
     #print(secatalog1,secatalog2)
     if 'BOK' in image1:
-        defaultcat = "default.sex.BOK"
-        #defaultcat = "default.sex.HDI"
+
+        #defaultcat = "default.sex.BOK"
+        defaultcat = "default.sex.HDI"
     elif 'INT' in image1:
         defaultcat = "default.sex.INT"
     else:
